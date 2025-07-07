7b1c2dac97af3fa654c356bfb1b6be63e53c0240
Yogeshbisht65 <152010856+Yogeshbisht65@users.noreply.github.com>
2025-07-07 21:38:21 +0530

    final commit

--- frontend/src/pages/Customize2.jsx
+++ frontend/src/pages/Customize2.jsx
@@ -37,7 +37,7 @@ return (
     <div className='w-full h-[100vh] bg-gradient-to-t from-[black] to-[#030353] flex justify-center items-center flex-col p-[20px] relative'>
         <IoMdArrowRoundBack className='absolute top-[30px] left-[30px] text-white cursor-pointer w-[25px] h-[25px]' onClick={()=> navigate("/customize")} />
         <h1 className='text-white text-[30px] text-center mb-[20px]'>Enter Your <span className='text-blue-200'>Assistant Name</span></h1>
-        <input type="text" placeholder='eg. Jarvis' className='w-full max-w-[600px] h-[60px] outline-none border-2 border-white bg-transparent text-white placeholder-gray-300 px-[20px] py-[10px] rounded-full text-[19px] mt-[10px]' required onChange={(e)=> setAssistantName(e.target.value)} value={assistantName} />
+        <input type="text" placeholder='eg. Aura' className='w-full max-w-[600px] h-[60px] outline-none border-2 border-white bg-transparent text-white placeholder-gray-300 px-[20px] py-[10px] rounded-full text-[19px] mt-[10px]' required onChange={(e)=> setAssistantName(e.target.value)} value={assistantName} />
         {assistantName && <button className="min-w-[300px] h-[50px] rounded-full bg-white text-black border-2 border-black text-[20px] font-semibold shadow-md hover:shadow-xl hover:scale-105 transition-all duration-300 ease-in-out mt-[20px]" disabled={loading}
     onClick={() => {
         handleUpdateAssistant()
