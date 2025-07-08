bf734bb58c99a847b562b214924deb6fac141a41
Yogeshbisht65 <152010856+Yogeshbisht65@users.noreply.github.com>
2025-07-08 10:39:09 +0530

    frontend url

--- backend/index.js
+++ backend/index.js
@@ -10,17 +10,22 @@ import geminiResponse from "./gemini.js";
 
 const app = express();
 app.use(cors({
-    origin:"http://localhost:5173",
-    credentials:true
+    origin: [
+        "http://localhost:5173",
+        "https://aura-e8zo.vercel.app"
+    ],
+    credentials: true
 }));
+
 const port = process.env.PORT || 5000;
+
 app.use(express.json());
 app.use(cookieParser());
-app.use("/api/auth",authRouter);
-app.use("/api/user",userRouter);
 
-app.listen(port,() => {
-    connectDb();
-    console.log("Server started");
-    
-})
+app.use("/api/auth", authRouter);
+app.use("/api/user", userRouter);
+
+app.listen(port, async () => {
+    await connectDb();
+    console.log(`✅ Server started on port ${port}`);
+});
