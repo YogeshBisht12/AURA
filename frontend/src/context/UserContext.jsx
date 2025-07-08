edad6ff4326d7839e8aa02ceae49b341f47f859f
Yogeshbisht65 <152010856+Yogeshbisht65@users.noreply.github.com>
2025-07-08 10:46:39 +0530

    completed

--- frontend/src/context/UserContext.jsx
+++ frontend/src/context/UserContext.jsx
@@ -4,7 +4,7 @@ import axios from 'axios';
 export const userDataContext = createContext();
 
 function UserContext({ children }) {
-const serverUrl = "http://localhost:8000";
+const serverUrl = "http://aura-production-72fb.up.railway.app";
 const [userData,setUserData] = useState(null);
 const [frontendImage,setFrontendImage] = useState(null);
 const [backendImage,setBackendImage] = useState(null);
