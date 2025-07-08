c15f04982ca6aabc0cae1f67f240b0a268f1bc65
Yogeshbisht65 <152010856+Yogeshbisht65@users.noreply.github.com>
2025-07-08 09:26:00 +0530

    final

--- backend/index.js
+++ backend/index.js
@@ -6,21 +6,23 @@ import authRouter from "./routes/auth.routes.js";
 import cookieParser from "cookie-parser";
 import cors from "cors";
 import userRouter from "./routes/user.routes.js";
-import geminiResponse from "./gemini.js";
+
 
 const app = express();
+
 app.use(cors({
-    origin:"http://localhost:5173", "https://aura-five-kappa.vercel.app",
-    credentials:true
+    origin: ["http://localhost:5173", "https://aura-five-kappa.vercel.app"],
+    credentials: true
 }));
+
 const port = process.env.PORT || 5000;
+
 app.use(express.json());
 app.use(cookieParser());
-app.use("/api/auth",authRouter);
-app.use("/api/user",userRouter);
+app.use("/api/auth", authRouter);
+app.use("/api/user", userRouter);
 
-app.listen(port,() => {
+app.listen(port, () => {
     connectDb();
     console.log("Server started");
-    
-})
+});
