df331b5bffd97f677198f2c824ac8625b03b6deb
Yogeshbisht65 <152010856+Yogeshbisht65@users.noreply.github.com>
2025-07-09 09:07:38 +0530

    done

--- backend/index.js
+++ backend/index.js
@@ -10,7 +10,7 @@ import geminiResponse from "./gemini.js";
 
 const app = express();
 app.use(cors({
-    origin:["http://localhost:5173","https://aura-sigma-rose.vercel.app"],
+    origin:["http://localhost:5173","https://aura-lkth.vercel.app"],
     credentials:true
 }));
 const port = process.env.PORT || 5000;
