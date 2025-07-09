ae76067c732faf8abc84ea45e3f661db2a99f43b
Yogeshbisht65 <152010856+Yogeshbisht65@users.noreply.github.com>
2025-07-09 10:20:53 +0530

    restart

--- backend/index.js
+++ backend/index.js
@@ -9,25 +9,10 @@ import userRouter from "./routes/user.routes.js";
 import geminiResponse from "./gemini.js";
 
 const app = express();
-const allowedOrigins = [
-  "http://localhost:5173", // for local dev
-  "https://https://aura-flame-ten.vercel.app", // your deployed frontend
-];
-
 app.use(cors({
-  origin: function (origin, callback) {
-    // Allow requests with no origin (e.g., curl, Postman, or server-side)
-    if (!origin) return callback(null, true);
-    if (allowedOrigins.includes(origin)) {
-      callback(null, true);
-    } else {
-      callback(new Error("Not allowed by CORS"));
-    }
-  },
-  credentials: true
+    origin:"http://localhost:5173",
+    credentials:true
 }));
-
-
 const port = process.env.PORT || 5000;
 app.use(express.json());
 app.use(cookieParser());
--- frontend/src/context/UserContext.jsx
+++ frontend/src/context/UserContext.jsx
@@ -4,7 +4,7 @@ import axios from 'axios';
 export const userDataContext = createContext();
 
 function UserContext({ children }) {
-const serverUrl = import.meta.env.VITE_SERVER_URL;
+const serverUrl = "http://localhost:8000";
 const [userData,setUserData] = useState(null);
 const [frontendImage,setFrontendImage] = useState(null);
 const [backendImage,setBackendImage] = useState(null);
