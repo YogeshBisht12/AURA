82d665516ce6d14571fd403049291c8cef08f042
Yogeshbisht65 <152010856+Yogeshbisht65@users.noreply.github.com>
2025-07-10 08:48:13 +0530

    new

--- backend/middlewares/isAuth.js
+++ backend/middlewares/isAuth.js
@@ -5,7 +5,7 @@ const isAuth = async (req,res,next) => {
         if(!token){
             return res.status(400).json({message:"token not found"});
         }
-        const verifyToken = await jwt.verify(token,process.env.JWT_SECRET);
+        const verifyToken = jwt.verify(token, process.env.JWT_SECRET);
         req.userId = verifyToken.userId;
 
         next();
